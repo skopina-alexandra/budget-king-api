9686d3cbc163a9bd696d0c37613b0e6f618a2b02
Alexandra Skopina <skopina.aa@outlook.com>
2024-04-16 15:55:23 +1000

    create relationships between users and categories

--- core/models/category.py
+++ core/models/category.py
@@ -1,7 +1,11 @@
 from .base import Base
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from typing import Literal, get_args
 from sqlalchemy import Enum, ForeignKey
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .user import User
 
 CategoryType = Literal["income", "outcome"]
 
@@ -18,3 +22,4 @@ class Category(Base):
     user_id: Mapped[int] = mapped_column(
         ForeignKey("users.id"),
     )
+    user: Mapped["User"] = relationship(back_populates="categories")
--- core/models/user.py
+++ core/models/user.py
@@ -1,8 +1,12 @@
 from .base import Base
 from sqlalchemy import DateTime
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 from sqlalchemy.sql import func
 from datetime import datetime
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from .category import Category
 
 
 class User(Base):
@@ -10,3 +14,4 @@ class User(Base):
     created_at: Mapped[datetime] = mapped_column(
         DateTime(timezone=True), server_default=func.now()
     )
+    categories: Mapped[list["Category"]] = relationship(back_populates="user")
