9066cbe984956609ab4a916b52a1c9de2d196a1d
Alexandra Skopina <skopina.aa@outlook.com>
2024-04-17 11:04:24 +1000

    remove hello-world route

--- main.py
+++ main.py
@@ -14,10 +14,5 @@ app = FastAPI(lifespan=lifespan)
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
-
-
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
