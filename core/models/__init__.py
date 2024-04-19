90be4a98522a378e8ba418a31c5d47575c754471
Alexandra Skopina <skopina.aa@outlook.com>
2024-04-19 10:25:39 +1000

    import models

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -6,6 +6,8 @@ __all__ = (
     "Category",
     "CategoryType",
     "Interval",
+    "Period",
+    "Record",
 )
 
 from .base import Base
@@ -13,3 +15,5 @@ from .db_helper import db_helper, DatabaseHelper
 from .user import User
 from .category import Category, CategoryType
 from .interval import Interval
+from .period import Period
+from .record import Record
