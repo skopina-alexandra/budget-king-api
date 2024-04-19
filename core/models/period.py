f487d4d0f63d98bc056a39593546ecc82f96f460
Alexandra Skopina <skopina.aa@outlook.com>
2024-04-19 10:15:50 +1000

    add period table

---
+++ core/models/period.py
@@ -0,0 +1,19 @@
+from sqlalchemy import ForeignKey
+from datetime import datetime
+from sqlalchemy import DateTime, Computed
+from .base import Base
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+
+class Period(Base):
+    user_id: Mapped[int] = mapped_column(
+        ForeignKey("users.id"),
+    )
+    start: Mapped[datetime] = mapped_column(
+        DateTime(timezone=True),
+    )
+    interval_id: Mapped[int] = (
+        mapped_column(
+            ForeignKey("intervals.id"),
+        ),
+    )
