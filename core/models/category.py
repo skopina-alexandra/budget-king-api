fd724557db468fd53d73928a7e0636c2ee8e14bb
Alexandra Skopina <skopina.aa@outlook.com>
2024-04-19 21:15:06 +1000

    add "savings" category type

--- core/models/category.py
+++ core/models/category.py
@@ -7,7 +7,7 @@ from typing import TYPE_CHECKING
 if TYPE_CHECKING:
     from .user import User
 
-CategoryType = Literal["income", "outcome"]
+CategoryType = Literal["income", "outcome", "savings"]
 
 
 class Category(Base):
