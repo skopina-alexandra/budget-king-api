ae7e782ee1d945d397f847d26e296cdf0f97321d
Alexandra Skopina <skopina-aa@yandex-team.ru>
2025-04-28 13:03:39 +1000

    setup logger

--- cmd/budget-king/main.go
+++ cmd/budget-king/main.go
@@ -2,10 +2,40 @@ package main
 
 import (
 	config_pkg "budget-king/internal/config"
-	"fmt"
+	"log/slog"
+	"os"
 )
 
 func main() {
 	config := config_pkg.MustLoad()
-	fmt.Println(config)
+	logger := setupLogger(config.Mode)
+	logger.Info("staring budget-king-api", slog.String("mode", config.Mode))
+	logger.Debug("debug messages are enabled")
+}
+
+const (
+	modeLocal = "local"
+	modeDev   = "dev"
+	modeProd  = "prod"
+)
+
+func setupLogger(mode string) *slog.Logger {
+	var logger *slog.Logger
+
+	switch mode {
+	case modeLocal:
+		logger = slog.New(
+			slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case modeDev:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelDebug}),
+		)
+	case modeProd:
+		logger = slog.New(
+			slog.NewJSONHandler(os.Stdout, &slog.HandlerOptions{Level: slog.LevelInfo}),
+		)
+
+	}
+	return logger
 }
